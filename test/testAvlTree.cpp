901516dad0c448fdf920fa444d7fd84dfbcd0b8e
Patrick Kuntz <soi.chiro@hotmail.de>
2017-12-18 14:35:11 +0100

    Added the AvlTree Header File Added the AvlTree CMakeLists.txt File Started with the AvlTree Source File
    Added the TestFolder

---
+++ test/testAvlTree.cpp
@@ -0,0 +1,13 @@
+#include "testAvlTree.h"
+#include <memory>
+
+using namespace std;
+
+// empty tree
+/*
+TEST(AvlTreeTest, Empty_Tree) {
+AvlTree a;
+EXPECT_EQ(nullptr, a.preorder());
+EXPECT_EQ(nullptr, a.inorder());
+EXPECT_EQ(nullptr, a.postorder());
+}*/
---
+++ test/testAvlTree.h
@@ -0,0 +1,16 @@
+//
+// Created by soich on 18.12.2017.
+//
+
+#ifndef BLATT_7_AUFGABE_1_MEGUSTA_TESTAVLTREE_H
+#define BLATT_7_AUFGABE_1_MEGUSTA_TESTAVLTREE_H
+
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "bintree/AvlTree.h"
+
+class AvlTreeTest : public ::testing::Test {
+};
+
+#endif //BLATT_7_AUFGABE_1_MEGUSTA_TESTAVLTREE_H
